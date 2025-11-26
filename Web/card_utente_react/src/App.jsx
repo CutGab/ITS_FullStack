3ea09477934e1f6062aefccfccc38f2426e8ca6b
CutGab <gabriele.cutolo2003@gmail.com>
2025-11-26 08:57:29 +0100

    Fix

---
+++ Web/card_utente_react/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Postgrid from './postgrid'
+
+function App() {
+
+  return (
+    <>
+    <div>
+
+      <Postgrid />
+
+    </div>
+    </>
+  )
+}
+
+export default App
